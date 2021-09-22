e2583dc46d31b4949cfe6a1b7d3b564670e2d1e4
Carlos Zabala <cgzabala@gmail.com>
2021-09-22 00:44:02 -0500

    Primera Versión de la Galería de Cohetes

---
+++ Shared/Entity/Rocket.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RocketProy.Shared.Entity
+{
+    public class Rocket
+    {
+        public string RocketName {get;set;}
+        public string RocketDescription {get;set;}
+        public string SpaceAgency {get;set;}
+        public string RocketImage {get;set;}
+        public DateTime PublicationDate {get;set;}
+    }
+}
